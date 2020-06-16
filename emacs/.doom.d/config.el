eb3a511b0efaa3db73035803334888d0faab142d
Øyvind Monsen <yvind.monsen@gmail.com>
2020-06-16 09:37:48 +0200

    Changed emacs font

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -16,7 +16,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "SourceCodePro" :size 11))
+(setq doom-font (font-spec :family "Consolas Regular" :size 11))
 
 (use-package avy
   :ensure t)
