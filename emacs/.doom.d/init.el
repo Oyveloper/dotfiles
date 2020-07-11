1fa8086317c2170555c929799cee46a05e4c7ef5
Øyvind Monsen <yvind.monsen@gmail.com>
2020-07-11 18:45:23 +0200

    everything succccccks

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -124,7 +124,7 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        (java +meghanada) ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)       ; all(hope(abandon(ye(who(enter(here))))))
+       ;;(javascript +lsp)       ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
@@ -159,7 +159,6 @@
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
        (swift +lsp)            ; who asked for emoji variables?
-       tcl
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
 
