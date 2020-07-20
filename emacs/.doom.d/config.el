2ebd5340e44dd424f68beb590fafbda26b753d72
Øyvind Monsen <yvind.monsen@gmail.com>
2020-07-20 11:21:31 +0200

    Trying company-box again

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -137,6 +137,10 @@
    company-selection-wrap-around t
    ))
 
+(use-package company-box
+  :ensure t
+  :hook (company-mode . company-box-mode))
+
 (use-package lsp
   :hook
   (js2-mode . lsp)
@@ -220,3 +224,17 @@
 ;; they are implemented.
 
 (evilem-default-keybindings "ø")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-box yasnippet-snippets tide popup-kill-ring lsp-ui lsp-python-ms lsp-java js-doc js-auto-beautify flycheck-tcl css-autoprefixer company-lsp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
